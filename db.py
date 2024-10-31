75b015402d398efffb2795d0728b8c2d3819a83e
Laura Pantoja <113263904+lipantoja@users.noreply.github.com>
2024-10-30 20:23:32 -0400

    Add update action

--- db.py
+++ db.py
@@ -29,7 +29,8 @@ def initial_setup():
     print("Table created successfully")
 
     pillows_seed_data = [
-        ("1st pillow", "https://cdn11.bigcommerce.com/s-r7rkk91ha4/images/stencil/1280x1280/products/440/3843/Garfield_1__36923.1729699225.jpg?c=1", "Garfield the Cat, as a pillow", "Medium")
+        ("1st pillow", "https://cdn11.bigcommerce.com/s-r7rkk91ha4/images/stencil/1280x1280/products/440/3843/Garfield_1__36923.1729699225.jpg?c=1", "Garfield the Cat, as a pillow", "Medium"),
+        ("2nd pillow", "https://morismos.com/cdn/shop/files/capybara-stuffed-animals-Capybara-plush-toys-long-bed-throw-pillows-cheap-plushies-sofa-body-pillows-bulk-stuffed-animals-wholesale-plush-toys-birthday-gift-guide.jpg?v=1714985781", "Capybara pillow, suitable for all ages.", "Small")
     ]
     conn.executemany(
         """
@@ -79,3 +80,17 @@ def pillows_find_by_id(id):
         (id,),
     ).fetchone()
     return dict(row)
+
+def pillows_update_by_id(id, name, image_url, description, size):
+    conn = connect_to_db()
+    row = conn.execute(
+        """
+        UPDATE pillows SET name = ?, image_url = ?, description = ?, size = ?
+        WHERE id = ?
+        RETURNING *
+        """,
+        (name, image_url, description, size, id),
+    ).fetchone()
+    conn.commit()
+    return dict(row)
+
